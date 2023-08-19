3eda8fe992edeb828f99287536a13e599d58e171
Sourik Bhuiya <sourikbhuiya@gmail.com>
2023-08-19 15:23:02 +0530

    Userschema Updated

---
+++ models/User.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema, mongo } from "mongoose"
+
+const userSchema= new mongoose.Schema({
+    email:{type:String, required:true,unique:true},
+    password:{type:String,required: true},
+    firstname: {type: String, required: true},
+    lastname: {type: String,require: true},
+    phone: {type:Number,require: true},
+    designation:{type:String, required:true},
+    organization:{type:String,required:true}
+});
+
+const User= mongoose.model('User',userSchema);
+
+
+module.exports = User;
+

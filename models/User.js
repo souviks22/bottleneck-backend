eaa0c46ab4b4fb44a0d868768b44abff7e7d1dfe
Sourik Bhuiya <sourikbhuiya@gmail.com>
2023-08-19 17:23:44 +0530

    Updated Userschema

--- models/User.js
+++ models/User.js
@@ -1,16 +1,17 @@
 import mongoose, { Schema, mongo } from "mongoose"
-
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const userSchema= new mongoose.Schema({
-    email:{type:String, required:true,unique:true},
-    password:{type:String,required: true},
-    firstname: {type: String, required: true},
-    lastname: {type: String,required: true},
-    phone: {type:Number,required: true},
+    email:{type:String, required:true,unique:true,match:emailRegex},
+    password:{type:String,required: true,match:passwordRegex},
+    firstname: {type: String, required: true,minlength:3 },
+    lastname: {type: String,required: true,minlength:2},
+    phone: {type:Number,required: true,minlength:10,maxlength:10},
     designation:{type:String, required:true},
     organization:{type:String,required:true}
 });
 
 const User= mongoose.model('User',userSchema);
 
-module.exports = User;
+export default User;
 

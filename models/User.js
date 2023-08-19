a5237104bf63dbb50947c8f41623dacf39e75aa1
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-08-19 19:36:09 +0530

    Required fields are set in User schema

--- models/User.js
+++ models/User.js
@@ -1,17 +1,41 @@
-import mongoose, { Schema, mongo } from "mongoose"
-const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-const userSchema= new mongoose.Schema({
-    email:{type:String, required:true,unique:true,match:emailRegex},
-    password:{type:String,required: true,match:passwordRegex},
-    firstname: {type: String, required: true,minlength:3 },
-    lastname: {type: String,required: true,minlength:2},
-    phone: {type:Number,required: true,minlength:10,maxlength:10},
-    designation:{type:String, required:true},
-    organization:{type:String,required:true}
-});
+import { Schema, model } from "mongoose"
 
-const User= mongoose.model('User',userSchema);
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
 
-export default User;
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: emailRegex
+    },
+    password: {
+        type: String,
+        required: true,
+        match: passwordRegex
+    },
+    firstname: {
+        type: String,
+        minlength: 3
+    },
+    lastname: {
+        type: String,
+        minlength: 3
+    },
+    phone: {
+        type: Number,
+        minlength: 10,
+        maxlength: 10
+    },
+    designation: {
+        type: String
+    },
+    organization: {
+        type: String,
+        minlength: 5
+    }
+})
 
+const User = model('User', userSchema)
+export default User

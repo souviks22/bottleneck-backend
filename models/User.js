e1fc0582b41d4ab01c81844bdb41365a9c74126c
Sourik Bhuiya <sourikbhuiya@gmail.com>
2023-08-20 23:22:06 +0530

    required fields updated

--- models/User.js
+++ models/User.js
@@ -4,11 +4,11 @@ const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const userSchema= new mongoose.Schema({
     email:{type:String, required:true,unique:true,match:emailRegex},
     password:{type:String,required: true,match:passwordRegex},
-    firstname: {type: String, required: true,minlength:3 },
-    lastname: {type: String,required: true,minlength:2},
-    phone: {type:Number,required: true,minlength:10,maxlength:10},
-    designation:{type:String, required:true},
-    organization:{type:String,required:true}
+    firstname: {type: String,minlength:3 },
+    lastname: {type: String, minlength:2},
+    phone: {type:Number,min:1000000000,max:9999999999},
+    designation:{type:String},
+    organization:{type:String}
 });
 
 const User= mongoose.model('User',userSchema);

d38ef7d658dde74d97e029657c8a2fe0b280fbb7
Sourik Bhuiya <sourikbhuiya@gmail.com>
2023-08-29 14:21:20 +0530

    Error Handling Updated

--- models/errorhandler.js
+++
@@ -1,7 +0,0 @@
-function errorHandler(err, req, res, next) {
-    console.error(err); // Log the error for debugging purposes
-    
-    // Handle validation errors
-    
-
-  export default errorHandler;
